d9db3291ad6ee580fbf43bd294085a97ac5e3b01
Pietro Lorefice <pietro@develer.com>
2024-06-04 07:50:03 +0200

    chip8-esp32: graphics

--- chip8-esp32/src/main.rs
+++ chip8-esp32/src/main.rs
@@ -6,7 +6,15 @@ use core::mem::MaybeUninit;
 use chip8::core::Chip8;
 use esp_alloc as _;
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*};
+use esp_hal::{
+    clock::ClockControl,
+    delay::Delay,
+    gpio::IO,
+    peripherals::Peripherals,
+    prelude::*,
+    spi::{master::Spi, SpiMode},
+};
+use sh1106::{mode::GraphicsMode, NoOutputPin};
 
 #[global_allocator]
 static ALLOC: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
@@ -19,17 +27,36 @@ fn main() -> ! {
         ALLOC.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
     }
 
-    let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
-
     esp_println::logger::init_logger_from_env();
 
-    let mut chip8 = Chip8::with_rom(include_bytes!("../../res/roms/TETRIS"));
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::max(system.clock_control).freeze();
 
-    log::info!("CHIP-8 initialized!");
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let sclk = io.pins.gpio0;
+    let miso = io.pins.gpio2;
+    let mosi = io.pins.gpio4;
+    let cs = io.pins.gpio5;
+    let dc = io.pins.gpio1;
+
+    let spi = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks).with_pins(
+        Some(sclk),
+        Some(mosi),
+        Some(miso),
+        Some(cs),
+    );
+
+    let mut display: GraphicsMode<_> = sh1106::Builder::new()
+        .connect_spi(spi, dc.into_push_pull_output(), NoOutputPin::new())
+        .into();
+
+    display.init().unwrap();
+    display.flush().unwrap();
+
+    let delay = Delay::new(&clocks);
+
+    let mut chip8 = Chip8::with_rom(include_bytes!("../../res/roms/TETRIS"));
 
     loop {
         // Read and update key state
@@ -42,7 +69,15 @@ fn main() -> ! {
         chip8.tick();
 
         // Render to screen
-        // chip8.vpu();
+        display.clear();
+        for y in 0..32 {
+            for x in 0..64 {
+                if chip8.vpu().read((x, y)) {
+                    display.set_pixel(x.into(), y.into(), 1);
+                }
+            }
+        }
+        display.flush().unwrap();
 
         // Wait for 1/60th of a second
         // TODO: use a periodic timer instead
